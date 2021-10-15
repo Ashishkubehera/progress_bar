46de6c4915b4d5de3b69467d5003ffb01a6d8a20
Ashish Ku Behera <74497927+Ashishkubehera@users.noreply.github.com>
2021-10-16 00:59:57 +0530

    n
    jmhgk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Progressbar from './component/Progress_bar';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+   <div className="App">
+     <h3 className="heading">Progress Bar</h3>
+     <ProgressBar>
+      <ProgressBar striped variant="success" now={35} key={1} />
+      <ProgressBar variant="warning" now={20} key={2} />
+      <ProgressBar striped variant="danger" now={10} key={3} />
+    </ProgressBar>
+      <Progressbar bgcolor="orange" progress='30'  height={30} />
+      <Progressbar bgcolor="red" progress='60'  height={30} />
+      <Progressbar bgcolor="#99ff66" progress='50'  height={30} />
+      <Progressbar bgcolor="#ff00ff" progress='85'  height={30} />
+      <Progressbar bgcolor="#99ccff" progress='95'  height={30} />
+   </div>
+     
   );
 }
-
+  
 export default App;
